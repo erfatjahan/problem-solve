f7d74d4f144e0941fd6b6acf027274cc29c697d0
erfatjahan <129873061+erfatjahan@users.noreply.github.com>
2024-08-11 10:53:42 +0600

    Add files via upload

---
+++ Practice Problems/day 1/A - Xorry 1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+pair<int,int>abc(int n) {
+    int a=1,b=0,c=0;
+    while(n>>1!=0) {
+        a=a<<1;
+        if (n & 1 == 1) {
+            b = b | (1 << c);
+        }
+        n=n>>1;
+        c=c+1;
+    }
+    return make_pair(b, a);
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        pair<int, int>ab=abc(n);
+        cout<<ab.first<<" "<<ab.second<<endl;
+    }
+}
